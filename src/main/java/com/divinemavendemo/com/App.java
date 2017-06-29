71e25ea4727b8316182a6f23461a12d5d0a32607
shyam karuparty <shyamkaruparty@gmail.com>
2017-06-29 17:44:11 -0500

    Initiated repository

---
+++ src/main/java/com/divinemavendemo/com/App.java
@@ -0,0 +1,13 @@
+package com.divinemavendemo.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
